e43de1a28dc3bcb2ee2e7c6abddac470d3d391ce
JimmyNH3 <jimmy20060504@gmail.com>
2020-11-01 18:50:06 +0800

    20201029homework

---
+++ 755022/Fertilizer.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int date = 1, S, E, P;
+    cin >> S >> E >> P;
+    while (S < E && P != 0) {
+        if (date % 11 == 0 && date > 10) {
+            P--; 
+        } else if (date % 3 != 0 && date % 11 < 9) {
+            S += S / 10;
+        } else if (date % 3 == 0 && date % 11 < 9) {
+            S += S / 3;
+        }
+        date++;
+    }
+    if (P == 0) {
+        cout << "unsalable";
+    } else {
+    cout << date;
+    }
+}
