b6ef2064f652ace075f8d14401cbe2b1455cc04b
pigeo <anthony79@apps.ntpc.edu>
2020-11-05 20:24:52 +0800

    feat: add typesetting

--- 755293/Anniversary.cpp
+++ 755293/Anniversary.cpp
@@ -4,10 +4,14 @@ using namespace std;
 
 int main() {
     int prize, con, shop;
+
     cin >> prize;
+
     con=prize/1000;
     prize-=con*100;
+
     if(con%2==0) shop=0;
     else  shop=1;
+    
     cout << prize << " " << shop << endl ;
 }
